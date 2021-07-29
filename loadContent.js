c7edaf2341964ff8554d16c5511ac1408e537a5f
Brad tarversy <xuvaer@gmail.com>
2021-07-29 21:53:27 +0600

    forgot to add the HTML folder

--- loadContent.js
+++ loadContent.js
@@ -1,27 +1,15 @@
 const fs = require('fs');
 
-const aboutPage = fs.readFileSync('./Content/HTML/about.html', 'utf-8', 'r');
-const blogPage = fs.readFileSync('./Content/HTML/blog.html', 'utf-8', 'r');
-const contactPage = fs.readFileSync(
-  './Content/HTML/contact.html',
-  'utf-8',
-  'r'
-);
+const aboutPage = fs.readFileSync('./HTML/about.html', 'utf-8', 'r');
+const blogPage = fs.readFileSync('./HTML/blog.html', 'utf-8', 'r');
+const contactPage = fs.readFileSync('./HTML/contact.html', 'utf-8', 'r');
 
-const indexPage = fs.readFileSync('./Content/HTML/index.html', 'utf-8', 'r');
-const pricingPage = fs.readFileSync(
-  './Content/HTML/pricing.html',
-  'utf-8',
-  'r'
-);
+const indexPage = fs.readFileSync('./HTML/index.html', 'utf-8', 'r');
+const pricingPage = fs.readFileSync('./HTML/pricing.html', 'utf-8', 'r');
 
-const servicesPage = fs.readFileSync(
-  './Content/HTML/services.html',
-  'utf-8',
-  'r'
-);
+const servicesPage = fs.readFileSync('./HTML/services.html', 'utf-8', 'r');
 
-const workPage = fs.readFileSync('./Content/HTML/work.html', 'utf-8', 'r');
+const workPage = fs.readFileSync('./HTML/work.html', 'utf-8', 'r');
 
 module.exports = {
   aboutPage,
