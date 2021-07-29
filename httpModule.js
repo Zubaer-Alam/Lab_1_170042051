b965b6da1e03314ab66366a4f0a7cff2419d664a
Brad tarversy <xuvaer@gmail.com>
2021-07-29 21:46:57 +0600

    updated and did a bit of tinkering

--- httpModule.js
+++ httpModule.js
@@ -1,13 +1,24 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-  res.write('<h1>Hello</h2>');
-  res.end();
+/*const server = http.createServer((req, res) => {
+  //res.write();
+  res.end('<h1>Hello</h1>');
 });
+*/
 
-/* if i don't provide the res.end(), then the page will keep loading
+/*if i don't provide the res.end(), then the page will keep loading
 res.end() is a must for the response to reach the end
 without using res.write(code), i can directly use res.end(code)
 */
- 
-module.exports ={server}
+
+const server = http.createServer((req, res) => {
+  if (req.url == '/') {
+    res.end('<h1>This is base URL</h1>');
+  } else if (req.url == '/home') {
+    res.end('<h1>This is HomePage</h1>');
+  } else {
+    res.end("<h1>This page doesn't exist</h1><br><a href = '/'>Go To BASE</a>");
+  }
+});
+
+module.exports = { server };
